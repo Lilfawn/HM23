dfea868bf3923852e6faff42f12d98cd02e3a2e7
Lilfawn <158754282+Lilfawn@users.noreply.github.com>
2024-05-28 17:11:51 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col">
+        <h2>Characteristics:</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <ul class="list-group list-group-flush">
+          <character-item
+              v-for="character in characteristics"
+              :key="character.id"
+              :character="character"
+          ></character-item>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <pre>{{ characteristics }}</pre>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CharacterItem from './components/CharacterFile.vue';
+
+export default {
+  components: {
+    CharacterItem
+  },
+  data() {
+    return {
+      characteristics: [
+        { id: 1, title: 'Health', count: 0 },
+        { id: 2, title: 'Strength', count: 0 },
+        { id: 3, title: 'Intelligence', count: 0 },
+        { id: 4, title: 'Endurance', count: 0 },
+        { id: 5, title: 'Mana', count: 0 },
+        { id: 6, title: 'Armor', count: 0 },
+        { id: 7, title: 'Speed', count: 0 }
+      ]
+    };
+  }
+}
+</script>
+
---
+++ CharacterFile.vue
@@ -0,0 +1,28 @@
+<template>
+  <li class="list-group-item">
+    <div class="d-inline-block me-3">{{ character.title }}</div>
+    <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-outline-primary" @click="decreaseCount">-</button>
+      <button type="button" class="btn btn-primary">{{ character.count }}</button>
+      <button type="button" class="btn btn-outline-primary" @click="increaseCount">+</button>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    character: Object
+  },
+  methods: {
+    increaseCount() {
+      this.character.count++;
+    },
+    decreaseCount() {
+      if (this.character.count > 0) {
+        this.character.count--;
+      }
+    }
+  }
+}
+</script>
